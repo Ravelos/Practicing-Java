764393c6f6f78a096772d374f260fe4bd9759da4
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-05 22:08:31 -0300

    criar conta

--- Pratica/src/br/com/iteris/universidade/Main.java
+++ Pratica/src/br/com/iteris/universidade/Main.java
@@ -4,12 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Conta primeiraConta = new Conta(2345,78900);
+        primeiraConta.deposita(200);
+        System.out.println("A primeira conta começou com " + primeiraConta.getSaldo());
+
+        primeiraConta.deposita(500);
+        System.out.println("O saldo da primeira conta é " + primeiraConta.getSaldo());
 
 
-
-
-
-
-
+        Conta segundaConta = new Conta(3452,98767);
+        segundaConta.deposita(150);
+        System.out.println("O saldo da segunda conta é " + segundaConta.getSaldo());
     }
 }

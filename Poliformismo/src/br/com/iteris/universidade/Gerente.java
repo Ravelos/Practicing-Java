8d8d7c174d9d58bacc6705f858e76f82262cb6ae
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-06 20:59:03 -0300

    testing manager class

---
+++ Poliformismo/src/br/com/iteris/universidade/Gerente.java
@@ -0,0 +1,16 @@
+package br.com.iteris.universidade;
+
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public void setSenha(int senha){
+        this.senha = senha;
+    }
+
+    public boolean autenticaSenha(int senha){
+        if (this.senha == senha){
+            return true;
+        }
+        return false;
+    }
+}
--- Poliformismo/src/br/com/iteris/universidade/Main.java
+++ Poliformismo/src/br/com/iteris/universidade/Main.java
@@ -8,5 +8,23 @@ public class Main {
         jose.setNome("José Daniel");
         jose.setCpf("98765432");
         jose.setSalario(3500.00);
+
+        System.out.println("O salario de José é " + jose.getSalario());
+        System.out.println("O nome completo desse cliente é " + jose.getNome());
+
+
+        Gerente luiz = new Gerente();
+        luiz.setNome("Luiz Mastantuano");
+        luiz.setSalario(5000.00);
+        luiz.setSenha(3456);
+        luiz.setCpf("786544111");
+
+        System.out.println(luiz.getNome());
+        System.out.println(luiz.getSalario());
+
+
+        boolean passwordCheck = luiz.autenticaSenha(3421);
+        System.out.println(passwordCheck);
+
     }
 }

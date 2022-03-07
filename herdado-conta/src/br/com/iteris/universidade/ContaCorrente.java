0a5f96000c941d8b3d98a29f5756c6837299b47e
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-07 17:29:08 -0300

    practicing with override and super

---
+++ herdado-conta/src/br/com/iteris/universidade/ContaCorrente.java
@@ -0,0 +1,14 @@
+package br.com.iteris.universidade;
+
+public class ContaCorrente extends Conta{
+
+    public ContaCorrente(int agencia,int numero){
+        super(agencia, numero);
+    }
+
+    @Override
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valorASacar);
+    }
+}
---
+++ herdado-conta/src/br/com/iteris/universidade/ContaPoupanca.java
@@ -0,0 +1,8 @@
+package br.com.iteris.universidade;
+
+public class ContaPoupanca extends Conta{
+
+    public ContaPoupanca(int agencia, int numero){
+        super(agencia, numero);
+    }
+}

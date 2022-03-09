0452df1e59e404bb6e036ef1564eee924c484073
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-09 14:57:09 -0300

    working in compisitions

---
+++ bytebank-encapsulado/src/br/com/iteris/universidade/Atenticavel.java
@@ -0,0 +1,5 @@
+package br.com.iteris.universidade;
+
+public interface Atenticavel {
+
+}
--- bytebank-encapsulado/src/br/com/iteris/universidade/Cliente.java
+++ bytebank-encapsulado/src/br/com/iteris/universidade/Cliente.java
@@ -1,9 +1,25 @@
 package br.com.iteris.universidade;
 
-public class Cliente {
+public class Cliente implements Atenticavel{
     private String nome;
     private String cpf;
     private String profissao;
+
+    private AutenticacaoUtil autenticador;
+
+    public Cliente(){
+        this.autenticador = new AutenticacaoUtil();
+    }
+
+    public void setSenha(int senha){
+
+        this.autenticador.setSenha(senha);
+
+    }
+
+    public boolean autentica(int senha){
+        return this.autenticador.autentica(senha);
+    }
 
     public void setNome(String nome) {
         this.nome = nome;

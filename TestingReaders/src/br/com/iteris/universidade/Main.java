dc2754a3317181c75e32804e1366d5ec7776e7a0
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-25 10:53:23 -0300

    importing reader in general

--- TestingReaders/src/br/com/iteris/universidade/Main.java
+++ TestingReaders/src/br/com/iteris/universidade/Main.java
@@ -3,16 +3,26 @@ package br.com.iteris.universidade;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         FileInputStream fis = new FileInputStream("lorem.txt");
-        InputStreamReader isr = new InputStreamReader(fis);
+
+        // importing reader in a general way
+        Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
         String line = br.readLine();
 
-        System.out.println(line);
+
+        // printing everything
+
+        while (line != null){
+            System.out.println(line);
+            line = br.readLine();
+        }
+        br.close();
 
     }
 }

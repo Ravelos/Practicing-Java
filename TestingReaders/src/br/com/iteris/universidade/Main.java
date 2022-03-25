b39c8a21ed536970f612ce876775f57b1efd5c98
Oswaldo Castillo <oswaldo.castillo@iteris.com.br>
2022-03-25 11:18:49 -0300

    solving the exception

--- TestingReaders/src/br/com/iteris/universidade/Main.java
+++ TestingReaders/src/br/com/iteris/universidade/Main.java
@@ -1,13 +1,10 @@
 package br.com.iteris.universidade;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         FileInputStream fis = new FileInputStream("lorem.txt");
 
         // importing reader in a general way
